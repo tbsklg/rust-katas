0e414732f3d3049546df22776aef1b690e64a5bf
tbsklg <tbs.klg@gmail.com>
2025-05-28 06:00:00 +0200

    Update 5266876b8f4bf2da9b000362

--- who_likes_it/src/main.rs
+++ who_likes_it/src/main.rs
@@ -5,24 +5,10 @@ fn main() {
 fn likes(names: &[&str]) -> String {
     match names.len() {
         0 => "no one likes this".to_string(),
-        1 => format!("{} likes this", names.first().unwrap()),
-        2 => format!(
-            "{} and {} like this",
-            names.first().unwrap(),
-            names.get(1).unwrap()
-        ),
-        3 => format!(
-            "{}, {} and {} like this",
-            names.first().unwrap(),
-            names.get(1).unwrap(),
-            names.get(2).unwrap()
-        ),
-        _ => format!(
-            "{}, {} and {} others like this",
-            names.first().unwrap(),
-            names.get(1).unwrap(),
-            names.len() - 2
-        ),
+        1 => format!("{} likes this", names[0]),
+        2 => format!("{} and {} like this", names[0], names[1]),
+        3 => format!("{}, {} and {} like this", names[0], names[1], names[2],),
+        l => format!("{}, {} and {} others like this", names[0], names[1], l - 2),
     }
 }
 
