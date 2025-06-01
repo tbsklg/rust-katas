eedb0bff1df621e6ae116e376966bafe7db919a6
tbsklg <tbs.klg@gmail.com>
2025-06-01 06:00:00 +0200

    Update 523f5d21c841566fde000009

---
+++ array_diff/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
+    a.into_iter().filter(|x| !b.contains(x)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn returns_expected() {
+        assert_eq!(array_diff(vec![1, 2], vec![1]), vec![2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![1]), vec![2, 2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![2]), vec![1]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![]), vec![1, 2, 2]);
+        assert_eq!(array_diff(vec![], vec![1, 2]), vec![]);
+        assert_eq!(array_diff(vec![1, 2, 3], vec![1, 2]), vec![3]);
+    }
+}
