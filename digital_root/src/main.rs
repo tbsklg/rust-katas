5c78d2ab82764bd8212ee26ad4c7190727270995
Tobias Klug <tbs.klg@gmail.com>
2025-06-05 06:30:00 +0200

    Solve 541c8630095125aba6000c00

---
+++ digital_root/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("{:?}", digital_root(1234567));
+}
+
+fn digital_root(n: i64) -> i64 {
+    if n < 10 {
+        return n;
+    }
+
+    digital_root(
+        n.to_string()
+            .chars()
+            .map(|x| x.to_digit(10).unwrap() as i64)
+            .sum::<i64>(),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(digital_root(16), 7);
+    }
+}
